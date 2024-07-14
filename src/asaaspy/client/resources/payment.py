b0218bedbde610a5314cf8b99c3787c248ab8954
Jonhnatha Trigueiro <joepreludian@gmail.com>
2024-07-14 18:45:02 -0300

    add get method for payment

--- src/asaaspy/client/resources/payment.py
+++ src/asaaspy/client/resources/payment.py
@@ -8,3 +8,8 @@ class PaymentResource(AsaasResource):
         with self.get_client() as client:
             response = client.post("/v3/payments", json=payment.as_lean_dict())
             return PaymentViewSchema(**response.json())
+    
+    def get(self, payment_id) -> PaymentViewSchema:
+        with self.get_client() as client:
+            response = client.get(f"/v3/payments/{payment_id}")
+            return PaymentViewSchema(**response.json())
