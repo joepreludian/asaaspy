bc9604b4589d4e7c99e1ec45a35288fc57de3319
Jonhnatha Trigueiro <joepreludian@gmail.com>
2024-10-28 20:15:13 -0300

    chore: fix pre-commit

--- tests/transfer/test_transfer_resource.py
+++ tests/transfer/test_transfer_resource.py
@@ -7,5 +7,7 @@ class TestPaymentResource:
         assert isinstance(return_data.data[0], TransferItemViewSchema)
 
     def test_get_transfer_by_id(self, asaas_svc):
-        return_data = asaas_svc.transfer.get_by_id("93cbacf4-3bc3-4fd8-802a-f7fdc41922ea")
+        return_data = asaas_svc.transfer.get_by_id(
+            "93cbacf4-3bc3-4fd8-802a-f7fdc41922ea"
+        )
         assert isinstance(return_data, TransferItemViewSchema)
